2745a1e01eed83f70bf8403d1baac51be8c467d6
mostafijurr <Mostafijur.Rahman@brainstation23.com>
2021-04-28 09:28:17 +0600

    front-end project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/common/header/header.component';
+import { FooterComponent } from './component/common/footer/footer.component';
+import { ProductListComponent } from './component/products/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
