f222e1050c185f58b53daf622f9afae74eed2755
MostafijurJ <mrkajol550@gmail.com>
2021-05-10 22:47:56 +0600

    add: cart list details is processing

--- src/app/component/cart/cart-details/cart-details.component.ts
+++ src/app/component/cart/cart-details/cart-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CartItem} from '../../../classes/cart/cart-item';
+import {CartItemService} from '../../../services/cart/cart-item.service';
 
 @Component({
   selector: 'app-cart-details',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-details.component.css']
 })
 export class CartDetailsComponent implements OnInit {
+ cartItem: CartItem[] = [];
+ totalPrice: string = '';
+ totalQuantity: number = 0;
 
-  constructor() { }
+  constructor(private cartItemService : CartItemService) { }
 
   ngOnInit(): void {
+    this.listCartItemDetails();
   }
 
+  private listCartItemDetails() {
+    this.cartItem = this.cartItemService.cartItems;
+    this.cartItemService.totalPrice.subscribe(price=>{
+      this.totalPrice = price.toFixed(2);
+    })
+
+    this.cartItemService.totalQuantity.subscribe(quantity =>{
+      this.totalQuantity = quantity;
+    })
+
+    console.log(`price = ${this.totalPrice} --- quantity = ${this.totalQuantity}`)
+
+    this.cartItemService.computeTotals();
+  }
 }
